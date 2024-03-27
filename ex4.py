b5e075080cdb5be1f3bc27b1f9e0255efb8ed7b6
vtsvykh <v.tsvykh0@gmail.com>
2024-03-27 20:05:24 +0700

    add ex5

--- ex4.py
+++ ex4.py
@@ -1,6 +1,6 @@
 class User:
     """
-
+    Class of site users.
     """
 
     def __init__(self, id, nick_name, first_name, last_name=None, middle_name=None, gender=None):
@@ -13,12 +13,7 @@ class User:
 
     def update(self, **kwargs):
         for attr, value in kwargs.items():
-            self.attr = value
+            setattr(self, attr, value)
 
     def __str__(self):
-        return f'Пользователь: {self.first_name} {self.last_name}, псевдоним: {self.nick_name}, id: {self.id}'
-
-user1 = User(id=1, nick_name='user1', first_name='John', last_name='Doe')
-print(user1)
-user1.update(last_name='Smith', gender='male')
-print(user1)
+        return f'User: {self.first_name} {self.middle_name} {self.last_name}, gender: {self.gender}, nickname: {self.nick_name}, id: {self.id}'
---
+++ ex5.py
@@ -0,0 +1,33 @@
+class Game:
+    """
+
+    """
+
+    def __init__(self, score):
+        self.score = score
+
+    def __str__(self):
+        return self.score
+
+    def __repr__(self):
+        return self.__str__()
+
+    def ball_thrown(self, command, points):
+        self.score[command] += points
+
+    def get_score(self):
+        name_team = list(self.score.keys())
+        score_team = list(self.score.values())
+        for _ in range(len(name_team)):
+            print(f'{name_team[_]}: {score_team[_]}')
+
+        return list(self.score.values())
+
+    def get_winner(self):
+        score = self.get_score()
+        if score[0] > score[1]:
+            print(f'Winner: {list(self.score.keys())[0]}')
+        elif score[0] < score[1]:
+            print(f'Winner: {list(self.score.keys())[1]}')
+        else:
+            print('Draw!')
