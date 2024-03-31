7ac70839e79873c0f66bfbbe9d8488db76022556
vtsvykh <v.tsvykh0@gmail.com>
2024-03-31 14:27:37 +0700

    update ex 6

--- ex6.py
+++ ex6.py
@@ -6,6 +6,7 @@ class Point:
     def __init__(self, crd=(0, 0)):
         """
         The function sets attributes for an instance of a class.
+        :param crd: coordinates of the point
         """
         self.x = crd[0]
         self.y = crd[1]
@@ -13,20 +14,22 @@ class Point:
     def get_x(self):
         """
         The function allows you to find out the x coordinate.
+        :return: the x coordinate
         """
         return self.x
 
     def get_y(self):
         """
         The function allows you to find out the y coordinate.
+        :return: the y coordinate
         """
         return self.y
 
     def distance(self, other):
         """
-
-        :param other:
-        :return:
+        The function determines the distance between two points.
+        :param other: coordinates of another point
+        :return: distance between two points
         """
         dist = ((other.get_x() - self.x) ** 2 + (other.get_y() - self.y) ** 2) ** 0.5
 
@@ -35,6 +38,8 @@ class Point:
     def sum(self, other):
         """
         The function allows you to find out the sum of two points.
+        :param other: coordinates of another point
+        :return: the sum of the coordinates of two points
         """
         new_crd = (self.x + other.get_x(), self.y + other.get_y())
         return new_crd
@@ -42,5 +47,5 @@ class Point:
     def __str__(self):
         return f'({self.x}, {self.y})'
 
-p = Point()
-print(p)
+    def __repr__(self):
+        return self.__str__()
